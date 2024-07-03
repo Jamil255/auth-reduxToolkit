1f22fd52ebe5554b9dda04430625ead81dbe862d
unknown <jamilafzal255@gmail.com>
2024-07-03 21:46:30 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@ import Signup from './components/Signup'
 import Login from './components/Login'
 import 'react-toastify/dist/ReactToastify.css'
 import { Bounce, ToastContainer } from 'react-toastify'
-import Home from './components/Home'
+// import Home from './components/Home'
+import axios from 'axios'
+import { useEffect } from 'react'
+axios.defaults.withCredentials = true
 const App = () => {
+  const getCookiess = async () => {
+    try {
+      const Res = await axios.get('http://localhost:8080', {
+        withCredentials: true,
+      })
+      console.log(Res)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <>
       <Routes>
         <Route index element={<Login />} />
         <Route path={'/signup'} element={<Signup />} />
-        <Route path={'/home'} element={<Home />} />
+        {/* <Route path={'/home'} element={<Home />} /> */}
       </Routes>
-
+      <button onClick={getCookiess}>click</button>
       <ToastContainer
         position="top-right"
         autoClose={5000}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,13 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { BASE_URL } from '../config'
+// import axios from 'axios'
+// import React, { useEffect } from 'react'
+// import { BASE_URL } from '../config'
 
-const Home = () => {
-  useEffect( async() => {
-    const userRes = await axios.get(`${BASE_URL}/home`)
-    console.log(userRes)
-  }, [])
-  return <div></div>
-}
+// const Home = () => {
+//   useEffect( async() => {
+//     const userRes = await axios.get(`${BASE_URL}/home`)
+//     console.log(userRes)
+//   }, [])
+//   return <div></div>
+// }
 
-export default Home
+// export default Home
